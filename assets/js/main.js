b95d2f92ce594243778671c802a0ded5cf252afc
madhumithaa <madhumithaa1411@gmail.com>
2020-05-24 20:51:00 +0530

    css solved for modals

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,106 +1,88 @@
 //model display depending on screen orientation
 function alertUser() {
-    // function to handle events from listner
+  // function to handle events from listner
 
-    if (window.matchMedia("(orientation: portrait)").matches) {
-        // currently in portrait, changing to landscape
-        $("#portrait-modal").modal('hide')
-    }
-    if (window.matchMedia("(orientation: landscape)").matches) {
-        // currently in landscape, changing to portrait
-        $("#portrait-modal").modal('show')
-    }
+  if (window.matchMedia("(orientation: portrait)").matches) {
+    // currently in portrait, changing to landscape
+    $("#portrait-modal").modal("hide");
+  }
+  if (window.matchMedia("(orientation: landscape)").matches) {
+    // currently in landscape, changing to portrait
+    $("#portrait-modal").modal("show");
+  }
 }
 
 // works only on orientation change
 window.onorientationchange = function () {
-    alertUser() // calls the function on change | remember `on` and `after` are different
+  alertUser(); // calls the function on change | remember `on` and `after` are different
 };
 
 // works only on page load/init
 if (window.matchMedia("(orientation: portrait)").matches) {
-    // currently in portrait
-    $("#portrait-modal").modal('show')
+  // currently in portrait
+  $("#portrait-modal").modal("show");
 }
 
 // minimize modal
 $("#minimize-btn").click(function () {
-    $('.notification-modal-class').toggleClass('d-none')
+  $(".notification-modal-class").toggleClass("d-none");
 });
 
 //on load minimize notification modal
-$("#minimize-btn").click()
-
+$("#minimize-btn").click();
 
 function toggleAnnouncementsModal() {
-
-    $('#notification-modal-dialog').toggleClass('d-none')
+  $("#notification-modal-dialog").toggleClass("d-none");
 }
-//image resizer 
+//image resizer
 setInterval(function () {
-    $('map').imageMapResize();
+  $("map").imageMapResize();
 }, 1000);
 
 //close event modal
 function eventmodal() {
-    $("#eventmodal").toggleClass('d-none')
-
+  $("#eventmodal").toggleClass("d-none");
 }
 //btn click
 $("#events-modal-btn").click(function () {
-    eventmodal()
+  eventmodal();
 });
-
-
 
 //close event room modal
 function eventroommodal() {
-    $("#eventroommodal").toggleClass('d-none')
-
+  $("#eventroommodal").toggleClass("d-none");
 }
 //btn click
 $("#events-room-modal-btn").click(function () {
-    eventroommodal()
+  eventroommodal();
 });
 
 function eventroommodal() {
-    $("#eventroommodal").toggleClass('d-none')
-
+  $("#eventroommodal").toggleClass("d-none");
 }
-
-
 
 // connect with us modal
 function toggleConnectWithUsModal() {
-
-    $('#connect-with-us-modal-dialog').toggleClass('d-none')
+  $("#connect-with-us-modal-dialog").toggleClass("d-none");
 }
 
-//close  modal
-
-function ConnectWithUsmodal() {
-    $("#connect-with-us-modal-dialog").toggleClass('d-none')
-
-}
-//btn click
-$("#connect-with-us-modal-btn").click(function () {
-    ConnectWithUsmodal()
-});
-
-$('.tab-item').click(function () {
-    $("#connect-with-us-modal-dialog").addClass("d-none")
-    $("#eventroommodal").addClass("d-none")
-    $("#eventmodal").addClass("d-none")
-    $("#pdf-modal-dialog").addClass("d-none")
-});
-
+// $(".tab-item").click(function () {
+//   $("#connect-with-us-modal-dialog").addClass("d-none");
+//   $("#eventroommodal").addClass("d-none");
+//   $("#eventmodal").addClass("d-none");
+//   $("#pdf-modal-dialog").addClass("d-none");
+// });
 
 // pdf modal
-function openPdfModal() {
-    $('#pdf-modal-dialog').toggleClass('d-none')
-}
-
 function togglePdfModal() {
-
-    $('#pdf-modal-dialog').toggleClass('d-none')
+  $("#pdf-modal-dialog").toggleClass("d-none");
 }
+
+function initPageLoad() {
+  $("#pdf-modal-dialog").removeClass("d-none");
+  $("#pdf-modal-dialog").addClass("d-none");
+  $("#connect-with-us-modal-dialog").removeClass("d-none");
+  $("#connect-with-us-modal-dialog").addClass("d-none");
+}
+
+initPageLoad();
